6bf97076edcaf357e540b6c145b580bf2000fe6a
Nickolay Ilyushin <nickolay02@inbox.ru>
2018-08-26 20:51:43 +0300

    asdf

--- package.cpp
+++ package.cpp
@@ -1,9 +1,13 @@
 #include <particlepm.hpp>
 
 PPM_PACKAGE(p) {
-  p->name("particlepm");
+  p->name("ppm-test-repo");
   p->version(PPM::Version("v0.1.0"));
   p->description("Ignore this please");
   p->github(PPM::GitHub("handicraftsman", "ppm-test-repo"));
   p->license(PPM::License("UNLICENSED", ""));
+
+  PPM::GitHubPtr ppm_d = p->git_repo("handicraftsman", "../ppm-test-repo", std::vector<PPM::Selector> {
+    PPM::Selector::greatere(PPM::Version("v0.1.0")),
+  });
 }
