98f869696b7993e797bb0820b4fdd96e48781f78
Nickolay Ilyushin <nickolay02@inbox.ru>
2018-08-26 17:50:20 +0300

    asdf

---
+++ package.cpp
@@ -0,0 +1,9 @@
+#include <particlepm.hpp>
+
+PPM_PACKAGE(p) {
+  p->name("particlepm");
+  p->version(PPM::Version("v0.1.0"));
+  p->description("Ignore this please");
+  p->github(PPM::GitHub("handicraftsman", "ppm-test-repo"));
+  p->license(PPM::License("UNLICENSED", ""));
+}
