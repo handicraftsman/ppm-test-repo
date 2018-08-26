fd4ef58954c0deb67fdfbe633c35e82eb600075d
Nickolay Ilyushin <nickolay02@inbox.ru>
2018-08-26 20:54:33 +0300

    asdf

--- package.cpp
+++ package.cpp
@@ -2,11 +2,12 @@
 
 PPM_PACKAGE(p) {
   p->name("ppm-test-repo");
-  p->version(PPM::Version("v0.1.0"));
+  p->version(PPM::Version("v0.1.3"));
   p->description("Ignore this please");
   p->github(PPM::GitHub("handicraftsman", "ppm-test-repo"));
   p->license(PPM::License("UNLICENSED", ""));
 
+  // yes, cycle dependency test
   PPM::GitHubPtr ppm_d = p->github_repo("handicraftsman", "ppm-test-repo", std::vector<PPM::Selector> {
     PPM::Selector::greatere(PPM::Version("v0.1.0")),
   });
